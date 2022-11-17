edaa257453d2f96ad6b289902f412a1d0f117207
sanasulaikha01 <117096782+sanasulaikha01@users.noreply.github.com>
2022-11-17 11:53:45 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php    
+$numbers=array("200","300","30","57");    
+echo "Sum of array elements is: ";
+echo array_sum($numbers);
+?>    
+ 
+</body>
+</html>
